4634235e4b5180d1e89354aa965350b1f79bdfb6
Naveen Prajapati <prajapatinaveen7003@gmai.com>
2024-03-19 14:46:09 +0530

    complete

--- models/dbconfig.js
+++ models/dbconfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 exports.dbconnection = async() => {
     try{
-        mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("connection Established!")
     }catch(error){
         console.log(error.message);
